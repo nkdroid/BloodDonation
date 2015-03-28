f575b9ecca4ddc62bb675dbe47b01cb9d4665c59
Niti <niti1511@gmail.com>
2015-03-28 09:07:57 +0530

    -Niti

---
+++ app/src/main/java/com/nkdroid/blooddonation/MapsActivity.java
@@ -0,0 +1,33 @@
+package com.nkdroid.blooddonation;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        LatLng india = new LatLng(22.3000, 73.2003);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(india, 13));
+        map.addMarker(new MarkerOptions()
+                .title("Vadodara")
+                .snippet("Beautiful City")
+                .position(india));
+    }
+
+
+}
